b5bc1817b45c166ae6b390101ee883b9b73c421b
dione <dlamericaan@gmail.com>
2025-02-26 22:12:45 +0100

    de homepagina begin van de website

---
+++ fitnessweb.client/src/Componenten/HeaderComponent.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+function HeaderComponent() {
+    return (
+        <header className="header">
+            <div className="logo">
+                <a href="/">
+                    <img src="/logo.png" alt="Fitness & Education Logo" />
+                </a>
+            </div>
+            <nav>
+                <ul>
+                    <li><a href="#home">Home</a></li>
+                    <li><a href="#muscles">Spieren</a></li>
+                    <li><a href="#exercises">Oefeningen</a></li>
+                    <li><a href="#calculator">Calorie Calculator</a></li>
+                </ul>
+            </nav>
+            <div className="user-interactions">
+                <a href="#login" className="button">Login</a>
+                <a href="#start-training" className="button button-highlight">Start Training</a>
+            </div>
+        </header>
+    );
+}
+
+export default HeaderComponent;
---
+++ fitnessweb.client/src/Paginas/Homepagina.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Homepagina.css';
+import HeaderComponent from '../Componenten/HeaderComponent';
+
+const Homepagina = () => {
+    return (
+        <div className="homepagina">
+            <header>
+                <HeaderComponent/>
+            </header>
+            <main>
+            </main>
+            <footer>
+            </footer>
+
+        </div>
+    );
+};
+
+export default Homepagina;
