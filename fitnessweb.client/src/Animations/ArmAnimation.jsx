5031c8f5223311213e6b59483e3aa5b3226f4bdd
PCD1 <dlamericaan@gmail.com>
2025-04-16 00:40:24 +0200

    i tried heb de tricep ook geanimeerd maar krijg het for some reason neit op de website te zien. im tired

--- fitnessweb.client/src/Animations/ArmAnimation.jsx
+++ fitnessweb.client/src/Animations/ArmAnimation.jsx
@@ -1,54 +1,27 @@
 import React, { useRef } from 'react';
+import { useGLTF, useAnimations } from '@react-three/drei';
 import { useFrame } from '@react-three/fiber';
-import { useGLTF } from '@react-three/drei';
 
 const ArmAnimation = () => {
     const group = useRef();
-    const lowerArmRef = useRef();
-    const direction = useRef(1);
 
-    const { nodes, materials } = useGLTF('/Arm-demo.glb'); // zorg dat pad klopt
-    console.log(nodes);
-    useFrame((_, delta) => {
-        if (lowerArmRef.current) {
-            lowerArmRef.current.rotation.z += direction.current * delta;
-            const rot = lowerArmRef.current.rotation.z;
-            if (rot > 0.5) direction.current = -1;
-            if (rot < -0.1) direction.current = 1;
+   
+    const { scene, animations } = useGLTF('/Armanimation.glb'); 
+    const { actions, mixer } = useAnimations(animations, group);
+
+   
+    React.useEffect(() => {
+        if (actions && actions['Action']) { 
+            actions['Action'].reset().play();
         }
+    }, [actions]);
+
+    
+    useFrame((state, delta) => {
+        mixer?.update(delta);
     });
 
-    return (
-        <group ref={group} scale={1.5} position={[0, 0, 1.5]}>
-            {/* Upper Arm (stilstaand) */}
-            <mesh
-                geometry={nodes.UpperArm.geometry}
-                material={materials?.default || undefined}
-                material-color="white"
-            />
-
-            <mesh
-                ref={lowerArmRef}
-                geometry={nodes.LowerArm.geometry}
-                material={materials?.default || undefined}
-                material-color="white"
-            />
-
-            {/* Biceps */}
-            <mesh
-                geometry={nodes.Biceps_brachii.geometry}
-                material={materials.default}
-                material-color="red"
-            />
-
-            {/* Triceps */}
-            <mesh
-                geometry={nodes.Triceps_brachii.geometry}
-                material={materials.default}
-                material-color="lightgray"
-            />
-        </group>
-    );
+    return <primitive object={scene} ref={group} scale={1.5} position={[0,0, 1.5]} />;
 };
 
 export default ArmAnimation;
