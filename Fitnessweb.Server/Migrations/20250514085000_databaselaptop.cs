fe5dabc47bb27103e14a24f354888d67cdb66a8b
dione <dlamericaan@gmail.com>
2025-05-21 09:35:31 +0200

    set up databaselaptop

---
+++ Fitnessweb.Server/Migrations/20250514085000_databaselaptop.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Fitnessweb.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class databaselaptop : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
